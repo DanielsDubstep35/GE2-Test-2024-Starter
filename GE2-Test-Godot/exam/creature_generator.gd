a95021f4763c61541e77d49498a9a4edf048067c
DanielsDubstep35 <98469510+DanielsDubstep35@users.noreply.github.com>
2024-04-23 10:18:49 +0100

    Attempting to create NoiseWanderer and Harmonic
    Fixed all previous errors

--- GE2-Test-Godot/exam/creature_generator.gd
+++ GE2-Test-Godot/exam/creature_generator.gd
@@ -1,11 +1,13 @@
 extends Node3D
 
 var length = 5
-var frequency
-var start_angle
+var frequency = 1
+var start_angle = 1
 var base_size: float = 0
-var multiplier = 2
+var multiplier: int = 5
 var draw = DebugDraw3D
+
+var sin_size = 1
 
 var baseVector3D = Vector3(0, 0, 0)
 
@@ -15,10 +17,10 @@ func _ready():
 func _process(delta):
 	on_draw_gizmos()
 	for i in 180:
-		base_size = i/10
+		sin_size = i/10
 		if i == 90 || i == 179:
-			print(base_size)
+			print(sin_size)
 		
 func on_draw_gizmos():
-	DebugDraw3D.draw_sphere(Vector3.ZERO, base_size, Color.RED, 5)
-	DebugDraw3D.draw_box(Vector3.ZERO, Quaternion(5, 5, 5, 5), Vector3(base_size, base_size, base_size), Color.SKY_BLUE, 5)
+	DebugDraw3D.draw_sphere(Vector3.ZERO, sin_size, Color.RED, 5)
+	DebugDraw3D.draw_box(Vector3.ZERO, Quaternion(5, 5, 5, 5), Vector3(sin_size, sin_size, sin_size), Color.SKY_BLUE, 5)
